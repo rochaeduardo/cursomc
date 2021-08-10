e8339fcee5710f283f6f09eb9eb173752e66dc8b
Eduardo Rocha <eduardo.rocha2510@hotmail.com>
2021-08-10 19:59:23 -0300

    Criando repository e service para Categoria

---
+++ src/main/java/br/com/rochaeduardo/cursomc/repositories/CategoriaRepository.java
@@ -0,0 +1,11 @@
+package br.com.rochaeduardo.cursomc.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import br.com.rochaeduardo.cursomc.domain.Categoria;
+
+@Repository
+public interface CategoriaRepository extends JpaRepository<Categoria, Integer> {
+
+}
