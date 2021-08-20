24d5107d11b4992e922e8ed4c4c09204792e07a2
Eduardo Rocha <eduardo.rocha2510@hotmail.com>
2021-08-20 18:21:52 -0300

    Endpoint /pedidos/{id} disponibilizado

--- src/main/java/br/com/rochaeduardo/cursomc/domain/Produto.java
+++ src/main/java/br/com/rochaeduardo/cursomc/domain/Produto.java
@@ -2,7 +2,6 @@ package br.com.rochaeduardo.cursomc.domain;
 
 import java.io.Serializable;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
@@ -17,6 +16,7 @@ import javax.persistence.ManyToMany;
 import javax.persistence.OneToMany;
 
 import com.fasterxml.jackson.annotation.JsonBackReference;
+import com.fasterxml.jackson.annotation.JsonIgnore;
 
 @Entity
 public class Produto implements Serializable {
@@ -34,6 +34,7 @@ public class Produto implements Serializable {
 	@JoinTable(name = "PRODUTO_CATEGORIA", joinColumns = @JoinColumn(name = "produto_id"), inverseJoinColumns = @JoinColumn(name = "categoria_id"))
 	private List<Categoria> categorias = new ArrayList<>();
 
+	@JsonIgnore
 	@OneToMany(mappedBy = "id.produto")
 	private Set<ItemPedido> itens = new HashSet<>();
 
@@ -47,6 +48,7 @@ public class Produto implements Serializable {
 		this.preco = preco;
 	}
 
+	@JsonIgnore
 	public List<Pedido> getPedidos() {
 		List<Pedido> lista = new ArrayList<>();
 		for (ItemPedido x : itens) {
---
+++ src/main/java/br/com/rochaeduardo/cursomc/services/PedidoService.java
@@ -0,0 +1,22 @@
+package br.com.rochaeduardo.cursomc.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import br.com.rochaeduardo.cursomc.domain.Pedido;
+import br.com.rochaeduardo.cursomc.repositories.PedidoRepository;
+import br.com.rochaeduardo.cursomc.services.exceptions.ObjectNotFoundException;
+
+@Service
+public class PedidoService {
+
+	@Autowired
+	private PedidoRepository repo;
+
+	public Pedido buscar(Integer id) {
+		Optional<Pedido> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto de id: " + id + " não foi encontrado!"));
+	}
+}
