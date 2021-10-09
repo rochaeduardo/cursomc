9c61638f2f81fec5e3a34017b90241b9e74f972a
Eduardo Rocha <eduardo.rocha2510@hotmail.com>
2021-10-08 22:03:15 -0300

    Paginação com parametros opcionais

--- src/main/java/br/com/rochaeduardo/cursomc/services/CategoriaService.java
+++ src/main/java/br/com/rochaeduardo/cursomc/services/CategoriaService.java
@@ -5,6 +5,9 @@ import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import br.com.rochaeduardo.cursomc.domain.Categoria;
@@ -38,11 +41,16 @@ public class CategoriaService {
 		try {
 			repo.deleteById(id);
 		} catch (DataIntegrityViolationException e) {
-			throw new DataIntegrityException("Não é possível excluir uma categoria que possui produtos",e);
+			throw new DataIntegrityException("Não é possível excluir uma categoria que possui produtos", e);
 		}
 	}
-	
-	public List<Categoria> findAll(){
+
+	public List<Categoria> findAll() {
 		return repo.findAll();
+	}
+
+	public Page<Categoria> findPage(Integer page, Integer linesPerPage, String orderBy, String direction) {
+		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
+		return repo.findAll(pageRequest);
 	}
 }
