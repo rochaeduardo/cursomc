eca025ba5da6a29769c634ebb379a9156ca1bb9e
Eduardo Rocha <eduardo.rocha2510@hotmail.com>
2021-11-05 23:41:25 -0300

    validação sintática

--- src/main/java/br/com/rochaeduardo/cursomc/dto/CategoriaDTO.java
+++ src/main/java/br/com/rochaeduardo/cursomc/dto/CategoriaDTO.java
@@ -2,18 +2,25 @@ package br.com.rochaeduardo.cursomc.dto;
 
 import java.io.Serializable;
 
+import javax.validation.constraints.NotEmpty;
+
+import org.hibernate.validator.constraints.Length;
+
 import br.com.rochaeduardo.cursomc.domain.Categoria;
 
 public class CategoriaDTO implements Serializable {
 	private static final long serialVersionUID = 1L;
 
 	private Integer id;
+
+	@NotEmpty(message = "Preenchimento obrigatório")
+	@Length(min = 5, max = 80, message = "O tamanho deve ser entre 5 e 80 caracteres")
 	private String nome;
 
 	public CategoriaDTO() {
 	}
-	
-	public CategoriaDTO(Categoria obj){
+
+	public CategoriaDTO(Categoria obj) {
 		this.id = obj.getId();
 		this.nome = obj.getNome();
 	}
