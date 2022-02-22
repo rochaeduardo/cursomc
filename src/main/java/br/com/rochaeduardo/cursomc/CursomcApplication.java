46a20f4631e7090dfd3e0e8afd4ad8011251c3c2
Eduardo Rocha <eduardo.rocha2510@hotmail.com>
2022-02-22 19:08:31 -0300

    Criando profile de desenvolvimento

--- src/main/java/br/com/rochaeduardo/cursomc/CursomcApplication.java
+++ src/main/java/br/com/rochaeduardo/cursomc/CursomcApplication.java
@@ -1,18 +1,13 @@
 package br.com.rochaeduardo.cursomc;
 
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class CursomcApplication implements CommandLineRunner {
+public class CursomcApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
 	}
 
-	@Override
-	public void run(String... args) throws Exception {
-
-	}
 }
