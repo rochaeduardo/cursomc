f3375328ddcfa825cf22486301e24db8bf3d7327
Eduardo Rocha <eduardo.rocha2510@hotmail.com>
2022-03-04 19:24:08 -0300

    Configuracao inicial do Spring Security

--- src/main/java/br/com/rochaeduardo/cursomc/config/TestConfig.java
+++ src/main/java/br/com/rochaeduardo/cursomc/config/TestConfig.java
@@ -6,6 +6,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
 
 import br.com.rochaeduardo.cursomc.services.DBService;
 import br.com.rochaeduardo.cursomc.services.EmailService;
@@ -27,5 +29,10 @@ public class TestConfig {
 	@Bean
 	public EmailService emailService() {
 		return new MockEmailService();
+	}
+	
+	@Bean
+	public JavaMailSender jMS() {
+		return new JavaMailSenderImpl();
 	}
 }
