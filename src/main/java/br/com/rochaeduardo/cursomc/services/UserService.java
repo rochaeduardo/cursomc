7ae745c55da5abf05381c53cce12773ea63c47b5
Eduardo Rocha <eduardo.rocha2510@hotmail.com>
2022-04-11 22:08:24 -0300

    Refresh token

---
+++ src/main/java/br/com/rochaeduardo/cursomc/services/UserService.java
@@ -0,0 +1,15 @@
+package br.com.rochaeduardo.cursomc.services;
+
+import org.springframework.security.core.context.SecurityContextHolder;
+
+import br.com.rochaeduardo.cursomc.security.UserSS;
+
+public class UserService {
+	public static UserSS authenticated() {
+		try {
+			return (UserSS) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+		} catch (Exception e) {
+			return null;
+		}
+	}
+}
