1995a447d9bf30e8854faf387e8a9ad10230a273
Eduardo Rocha <eduardo.rocha2510@hotmail.com>
2022-04-11 21:48:59 -0300

    Restrição de conteúdo: cliente só recupera seus pedidos

--- src/main/java/br/com/rochaeduardo/cursomc/services/PedidoService.java
+++ src/main/java/br/com/rochaeduardo/cursomc/services/PedidoService.java
@@ -4,9 +4,13 @@ import java.util.Date;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import br.com.rochaeduardo.cursomc.domain.Cliente;
 import br.com.rochaeduardo.cursomc.domain.ItemPedido;
 import br.com.rochaeduardo.cursomc.domain.PagamentoComBoleto;
 import br.com.rochaeduardo.cursomc.domain.Pedido;
@@ -14,6 +18,8 @@ import br.com.rochaeduardo.cursomc.domain.enums.EstadoPagamento;
 import br.com.rochaeduardo.cursomc.repositories.ItemPedidoRepository;
 import br.com.rochaeduardo.cursomc.repositories.PagamentoRepository;
 import br.com.rochaeduardo.cursomc.repositories.PedidoRepository;
+import br.com.rochaeduardo.cursomc.security.UserSS;
+import br.com.rochaeduardo.cursomc.services.exceptions.AuthorizationException;
 import br.com.rochaeduardo.cursomc.services.exceptions.ObjectNotFoundException;
 
 @Service
@@ -33,10 +39,10 @@ public class PedidoService {
 
 	@Autowired
 	private ItemPedidoRepository itemPedidoRepository;
-	
+
 	@Autowired
 	private ClienteService clienteService;
-	
+
 	@Autowired
 	private EmailService emailService;
 
@@ -71,6 +77,18 @@ public class PedidoService {
 		emailService.sendOrderConfirmationHtmlEmail(obj);
 		return obj;
 	}
-	
-	
+
+	public Page<Pedido> findPage(Integer page, Integer linesPerPage, String orderBy, String direction) {
+
+		UserSS user = UserService.authenticated();
+		if (user == null) {
+			throw new AuthorizationException("Acesso Negado!");
+		}
+
+		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
+		Cliente cliente = clienteService.find(user.getId());
+
+		return repo.findByCliente(cliente, pageRequest);
+	}
+
 }
